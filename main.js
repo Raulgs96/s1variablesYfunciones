5c32730910ecec77bd583bca2b3a8b64635dd889
Raul Gomez <raulgs96@hotmail.com>
2023-10-12 22:40:45 -0600

    operaciones

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("hola koders")
+let a= 7
+let b=5
+
+let suma= a+b
+let resta= a-b
+let multiplicacion= a*b
+let division= a/b
+let modulo= a%b
+
+
+console.log("Resultado de la suma es:" +suma)
+console.log("Resultado de la resta es:"+ resta)
+console.log("Resultadode la multiplicacion  es:"+ multiplicacion)
+console.log("Resultado de la division es:"+ division)
+console.log("Resultado del modulo es:"+ modulo)
