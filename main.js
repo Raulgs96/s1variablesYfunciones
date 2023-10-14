87fc7beef10b00b466f3cbcc14138731372d03e4
Raul Gomez <raulgs96@hotmail.com>
2023-10-13 22:03:06 -0600

    Adding funtions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("hola koders")
+/*console.log("hola koders")
 let a= 7
 let b=5
 
@@ -13,4 +13,51 @@ console.log("Resultado de la suma es:" +suma)
 console.log("Resultado de la resta es:"+ resta)
 console.log("Resultadode la multiplicacion  es:"+ multiplicacion)
 console.log("Resultado de la division es:"+ division)
-console.log("Resultado del modulo es:"+ modulo)
+console.log("Resultado del modulo es:"+ modulo)*/
+
+function suma(a, b){
+    let resultsuma = a+b;
+    return resultsuma
+}
+
+function resta(a, b){
+let resultresta= a-b;
+    return resultresta
+}
+function multiplicacion(a, b){
+    let resultmultiplicacion= a*b;
+    return resultmultiplicacion
+}
+function division(a, b){
+    let resultdivision=a/b;
+    return resultdivision
+}
+function modulo(a, b){
+    let resultmodulo=a%b;
+    return resultmodulo
+}
+function mayorque(a,b){
+    if (a>b){
+        return " A Mayor";
+    } else if (a<b){
+        return " B Mayor";
+    } else{
+        return "son iguales";
+    }
+    
+}
+function parOnone (a){
+    if(a%2===0){
+        return "Par"
+    } else {
+        return "none"
+    }
+}
+
+console.log(suma(10,20))
+console.log(resta(10,20))
+console.log(multiplicacion(10,20))
+console.log(division(10,20))
+console.log(modulo(10,20))
+console.log(mayorque(20,20))
+console.log(parOnone(8))
