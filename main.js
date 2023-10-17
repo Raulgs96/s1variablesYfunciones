d7e50325fe54f255a1cf5ce0f021a3c9dc73dbb9
Raul Gomez <raulgs96@hotmail.com>
2023-10-16 21:42:02 -0600

    adding functions if

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ console.log("Resultadode la multiplicacion  es:"+ multiplicacion)
 console.log("Resultado de la division es:"+ division)
 console.log("Resultado del modulo es:"+ modulo)*/
 
-function suma(a, b){
+/*function suma(a, b){
     let resultsuma = a+b;
     return resultsuma
 }
@@ -60,4 +60,38 @@ console.log(multiplicacion(10,20))
 console.log(division(10,20))
 console.log(modulo(10,20))
 console.log(mayorque(20,20))
-console.log(parOnone(8))
+console.log(parOnone(8)) */
+
+/////////////////////////////////////EJERCICIOS EN CLASE///////////////////////////////////////////////////
+//////////////////////////////////////////////PRACTICA ESPECIES/////////////////////////////////////////////////////////////
+
+let pet
+function petName (pet) {
+   
+        if (pet=="perro"){
+            return "Tu perro se podria llamar Milaneso";
+        } else if (pet=="gato"){
+            return "Tu gato se podria llamar Michiflais";
+        } else if (pet=="ave"){
+                return "Tu ave se podria llamar piolin";
+        } else{
+            return "No conozco la especie";
+        }
+        
+}
+console.log(petName("pato"))
+///////////////////////////////////////////PRACTICA DADOS////////////////////////////////////////////////////////////////
+let dado1, dado2
+function tirarDado (dado1, dado2){
+    if ((dado1>=1 && dado1<=6) && (dado2>=1 && dado2<=6)){
+        if ((dado1==2 && dado2==2) || (dado1==4 && dado2==6) || ( dado1==1 && dado2==1) || (dado1==6 && dado2==4)){
+            return "GANASTE"
+        } else  {
+            return "SUERTE PARA LA PROXIMA PERDEDOR"
+        } 
+    } else {
+        return "NUMEROS INVALIDOS"
+    }
+}
+
+console.log (tirarDado(5,3))
