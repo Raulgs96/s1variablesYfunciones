7d1afff5013dc8b89bb1890f67fd06495939c9a8
Raul Gomez <raulgs96@hotmail.com>
2023-10-18 00:37:23 -0600

    text

---
+++ textSize.js
@@ -0,0 +1,13 @@
+function txtSize (txt, num ){
+    let newtxt=""
+    if (num<txt.length){
+        for (i=0; i<num; i++){
+            newtxt +=txt.at(i)
+        }
+        } else {
+      console.log("error")
+                }
+    
+    console.log(newtxt)
+ }
+ txtSize("abcdefghijklmnñ",5)
